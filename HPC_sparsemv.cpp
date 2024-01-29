a9ffe66bbe00d12cb798af00b11ded03b83fe16b
EdmundGoodman <egoodman3141@gmail.com>
2024-01-29 15:39:41 +0000

    Add documentation about sparse matrix structure

--- HPC_sparsemv.cpp
+++ HPC_sparsemv.cpp
@@ -57,14 +57,14 @@ using std::endl;
  *
  * @note In MPI, first call exchange_externals to get off-processor values of x.
  *
- * This implementation looks slightly non-traditional due to the design of the
- * sparse matrix data structure. `cur_vals` is a pointer to a slice of the
- * `list_of_vals` array, which hence contains an ordered array of the non-zero
- * values in the current row. `cur_inds` equivalently is an ordered array of the
- * indices of those values in the current row. `cur_nnz` is the number of
- * non-zeroes in the array (hence the length of both `cur_vals` and `cur_inds`
- * array slices, as the `list_of_vals/inds` arrays are non-homogenous in
- * sub0array length, and don't have termination to split the sub-arrays up).
+ * The input matrix to the function is stored in compressed-sparse column form.
+ * Hence, `cur_vals` is a pointer to a slice of the `list_of_vals` array, which
+ * then contains an ordered array of the non-zero values in the current row.
+ * `cur_inds` equivalently is an ordered array of the indices of those values in
+ * the current row. `cur_nnz` is the number of non-zeroes in the array (hence
+ * the length of both `cur_vals` and `cur_inds` array slices, as the
+ * `list_of_vals/inds` arrays are non-homogenous in sub-array length, and don't
+ * have termination to split the sub-arrays up).
  *
  * Hence, the matrix-vector multiplication is an array containing the products
  * of those non-zero values with their pairs in the input vector, for each rown
